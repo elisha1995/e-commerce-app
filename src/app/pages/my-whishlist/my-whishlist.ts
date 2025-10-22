93f919d7b0dc497245d7e929119e63f909708e7a
Elisha Samuel Gyamfi <elishasamuel01@gmail.com>
2025-10-22 22:19:17 +0000

    Add routing for products and wishlist pages, and create corresponding components

---
+++ src/app/pages/my-whishlist/my-whishlist.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-whishlist',
+  imports: [],
+  template: `
+    <p>
+      my-whishlist works!
+    </p>
+  `,
+  styles: ``
+})
+export default class MyWhishlist {
+
+}
