34b5bb80d2e3868251135f6858a881237de14587
Elisha Samuel Gyamfi <elishasamuel01@gmail.com>
2025-10-26 20:20:32 +0000

    Add BackButton component and integrate it into MyWhishlist page

--- src/app/pages/my-whishlist/my-whishlist.ts
+++ src/app/pages/my-whishlist/my-whishlist.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { BackButton } from "../../components/back-button/back-button";
 
 @Component({
   selector: 'app-my-whishlist',
-  imports: [],
+  imports: [BackButton],
   template: `
-    <p>
-      my-whishlist works!
-    </p>
+    <div class="mx-auto max-w-[1200px] py-6 px-4">
+      <app-back-button label="Continue Shopping" [navigateTo]="'/products/all'"></app-back-button>
+    </div>
   `,
   styles: ``
 })
